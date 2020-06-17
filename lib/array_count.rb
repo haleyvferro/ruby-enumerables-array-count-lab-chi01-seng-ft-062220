a4894dbbb180e0db2890436c0ac088ef37534ba8
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 00:17:35 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,8 @@
 def count_strings(array)
   total = 0 
   array.count do |element|
-    if element.is_a? String
+    if element.is_a? String 
+      total += 1
 end
 
 def count_empty_strings(array)
