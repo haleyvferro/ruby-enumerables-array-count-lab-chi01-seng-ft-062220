66cc736410cf261c6d013f9838c5f9066385f219
Haley Ferro <haleyvferro@gmail.com>
2020-06-17 00:18:21 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,5 +8,10 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  total = 0 
+  array.count do |element|
+    if element == '' 
+      total += 1 
+    end
+  end
 end
